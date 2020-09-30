24f9b61c8b0992d98c83df1aa9a4e61705683554
adinhobl <andrewdinhobl@gmail.com>
2020-09-29 21:46:26 -0500

    doing part 2 of first optimization problem

--- randomized-opt/opt1.py
+++ randomized-opt/opt1.py
@@ -1,6 +1,7 @@
 import mlrose_hiive as mlr
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 seed=121
 np.random.seed(seed=seed)
@@ -22,10 +23,10 @@ def toothymax(state, punishment=0):
         if state[i] in tracker:
             fitness += punishment
         elif state[i] % 2 == 0 and i % 2 == 0:
-            fitness += (state[i] * i)**0.25
+            fitness += (state[i] * i)**0.5
             tracker.append(state[i])
         elif state[i] % 2 != 0 and i % 2 != 0:
-            fitness += (state[i] * i)**0.25
+            fitness += (state[i] * i)**0.5
             tracker.append(state[i])
         else:
             fitness += punishment
@@ -33,11 +34,15 @@ def toothymax(state, punishment=0):
     
     return fitness
 
-print(f"== Setup ==\n")
+print(f"######### PART 1 #########\n")
+print(f"=== Setup ===\n")
 
-prob_len = 100
-attemps = 1000
+prob_len = 3
+attemps = 10000
+max_it = 2000
 print(f"  Problem Length: {prob_len}\n")
+
+print(f"  Max Fitness: {toothymax(np.arange(0,prob_len))}\n")
 
 init = np.random.choice(prob_len, size=prob_len, replace=False)
 
@@ -49,12 +54,13 @@ total_time = 0.0
 print(f"  Initialization:\n{init}")
 
 ## Randomized Hill Climbing
-print(f"\n== Randomized Hill Climbing ==")
+print(f"\n=== Randomized Hill Climbing ===")
 start = time.time()
 best_rhc_state, best_rhc_fitness, rhc_curve = mlr.random_hill_climb(problem, 
                                                           max_attempts=attemps,
                                                           random_state=seed, curve=True,
-                                                          init_state=init, restarts=10)
+                                                          init_state=init, restarts=1000,
+                                                          max_iters=max_it)
                                                           
 end = time.time()
 total_time += end - start
@@ -66,62 +72,66 @@ print(f"  Elapsed: {end - start}")
 # print(sorted(best_rhc_state))
 
 ## Simulated Annealing
-print(f"\n== Simulated Annealing - ExpDecay ==")
-schedule = mlr.ExpDecay()
-start = time.time()
-best_saE_state, best_saE_fitness, saE_curve = mlr.simulated_annealing(problem, schedule=schedule, 
-                                                          max_attempts=attemps, random_state=seed, 
-                                                          curve=True, init_state=init)
+# print(f"\n=== Simulated Annealing - ExpDecay ===")
+# schedule = mlr.ExpDecay()
+# start = time.time()
+# best_sa_state, best_sa_fitness, sa_curve = mlr.simulated_annealing(problem, schedule=schedule, 
+#                                                           max_attempts=attemps, random_state=seed, 
+#                                                           curve=True, init_state=init,
+#                                                           max_iters=max_it)
                                                           
-end = time.time()
-total_time += end - start
-print(f"  Fitness: {best_saE_fitness}")
-print(f"  Best State:\n{best_saE_state}")
-print(f"  Fitness Curve:\n{saE_curve}")
-print(f"  Elapsed: {end - start}")
+# end = time.time()
+# total_time += end - start
+# print(f"  Fitness: {best_sa_fitness}")
+# print(f"  Best State:\n{best_sa_state}")
+# print(f"  Fitness Curve:\n{sa_curve}")
+# print(f"  Elapsed: {end - start}")
 
-# print(sorted(best_saE_state))
+## print(sorted(best_sa_state))
 
 ## Simulated Annealing
-print(f"\n== Simulated Annealing - GeomDecay ==")
-schedule = mlr.GeomDecay()
-start = time.time()
-best_saG_state, best_saG_fitness, saG_curve = mlr.simulated_annealing(problem, schedule=schedule, 
-                                                          max_attempts=attemps, random_state=seed, 
-                                                          curve=True, init_state=init)
+# print(f"\n=== Simulated Annealing - GeomDecay ===")
+# schedule = mlr.GeomDecay()
+# start = time.time()
+# best_sa_state, best_sa_fitness, sa_curve = mlr.simulated_annealing(problem, schedule=schedule, 
+#                                                           max_attempts=attemps, random_state=seed, 
+#                                                           curve=True, init_state=init,
+#                                                           max_iters=max_it)
                                                           
-end = time.time()
-total_time += end - start
-print(f"  Fitness: {best_saG_fitness}")
-print(f"  Best State:\n{best_saG_state}")
-print(f"  Fitness Curve:\n{saG_curve}")
-print(f"  Elapsed: {end - start}")
+# end = time.time()
+# total_time += end - start
+# print(f"  Fitness: {best_sa_fitness}")
+# print(f"  Best State:\n{best_sa_state}")
+# print(f"  Fitness Curve:\n{sa_curve}")
+# print(f"  Elapsed: {end - start}")
 
-# print(sorted(best_saG_state))
+## print(sorted(best_sa_state))
 
 ## Simulated Annealing
-print(f"\n== Simulated Annealing - ArithDecay ==")
+print(f"\n=== Simulated Annealing - ArithDecay ===")
 schedule = mlr.ArithDecay()
 start = time.time()
-best_saA_state, best_saA_fitness, saA_curve = mlr.simulated_annealing(problem, schedule=schedule, 
+best_sa_state, best_sa_fitness, sa_curve = mlr.simulated_annealing(problem, schedule=schedule, 
                                                           max_attempts=attemps, random_state=seed, 
-                                                          curve=True, init_state=init)
+                                                          curve=True, init_state=init,
+                                                          max_iters=max_it)
                                                           
 end = time.time()
 total_time += end - start
-print(f"  Fitness: {best_saA_fitness}")
-print(f"  Best State:\n{best_saA_state}")
-print(f"  Fitness Curve:\n{saA_curve}")
+print(f"  Fitness: {best_sa_fitness}")
+print(f"  Best State:\n{best_sa_state}")
+print(f"  Fitness Curve:\n{sa_curve}")
 print(f"  Elapsed: {end - start}")
 
-# print(sorted(best_saA_state))
+# print(sorted(best_sa_state))
 
 ## Genetic Algorithm
-print(f"\n== Genetic Algorithm ==")
+print(f"\n=== Genetic Algorithm ===")
 start = time.time()
-best_ga_state, best_ga_fitness, ga_curve = mlr.genetic_alg(problem, pop_size=100, 
+best_ga_state, best_ga_fitness, ga_curve = mlr.genetic_alg(problem, pop_size=200, 
                                                           max_attempts=attemps, random_state=seed, 
-                                                          curve=True, mutation_prob=0.1)
+                                                          curve=True, mutation_prob=0.1,
+                                                          max_iters=max_it)
                                                           
 end = time.time()
 total_time += end - start
@@ -132,9 +142,126 @@ print(f"  Elapsed: {end - start}")
 
 # print(sorted(best_ga_state))
 
+## MIMIC
+print(f"\n=== MIMIC ===")
+start = time.time()
+best_m_state, best_m_fitness, m_curve = mlr.mimic(problem, pop_size=200, 
+                                                max_attempts=attemps, 
+                                                random_state=seed, curve=True,
+                                                max_iters=max_it)
+                                                          
+end = time.time()
+total_time += end - start
+print(f"  Fitness: {best_m_fitness}")
+print(f"  Best State:\n{best_m_state}")
+print(f"  Fitness Curve:\n{m_curve}")
+print(f"  Elapsed: {end - start}")
+
+# print(sorted(best_m_state))
+
+print(f"\n\nTotal Time Elapsed: {total_time}\n")
+
+## Plotting
+
+print("Plotting Part 1\n")
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(1,1,1)
+ax1.plot(rhc_curve, label="Randomized Hill Climbing")
+ax1.plot(sa_curve, label="Simulated Annealing")
+ax1.plot(ga_curve, label="Genetic Algorithm")
+ax1.plot(m_curve, label="MIMIC")
+
+plt.legend()
+plt.show()
+plt.savefig("Opt1Part1.png")
 
 
+print(f"######### PART 2 #########\n")
 
-print(f"\n\nTotal Time Elapsed: {total_time}")
+# test_range = [3,5,8,10,15,20,30,50,100,200,500,1000]
+test_range = [3,5,8,10,15,20]
+total_time = 0.0
 
-print(toothymax(np.arange(0,prob_len)))
+rhc_fitnesses = []
+sa_fitnesses = []
+ga_fitnesses = []
+m_fitnesses = []
+rhc_times = []
+sa_times = []
+ga_times = []
+m_times = []
+
+start = time.time()
+
+for i in test_range:
+    print(f"Running for subproblem size: {i}\n")
+
+    init = np.random.choice(i, size=i, replace=False)
+    problem = mlr.DiscreteOpt(length=i, fitness_fn=fitness_cust, 
+                          maximize=True, max_val=i)
+
+    ## Randomized Hill Climbing
+    sub_start = time.time()
+    best_rhc_state, best_rhc_fitness, rhc_curve = mlr.random_hill_climb(problem, 
+                                                            max_attempts=attemps,
+                                                            random_state=seed, curve=True,
+                                                            init_state=init, restarts=100)
+    sub_end = time.time()
+    rhc_fitnesses.append(best_rhc_fitness)
+    rhc_times.append(sub_end - sub_start)
+
+    ## Simulated Annealing
+    sub_start = time.time()
+    best_sa_state, best_sa_fitness, sa_curve = mlr.simulated_annealing(problem, schedule=schedule, 
+                                                            max_attempts=attemps, random_state=seed, 
+                                                            curve=True, init_state=init)
+    sub_end = time.time()
+    sa_fitnesses.append(best_sa_fitness)
+    sa_times.append(sub_end - sub_start)
+
+    ## Genetic Algorithm
+    sub_start = time.time()
+    best_ga_state, best_ga_fitness, ga_curve = mlr.genetic_alg(problem, pop_size=200, 
+                                                            max_attempts=attemps, random_state=seed, 
+                                                            curve=True, mutation_prob=0.1)                                           
+    sub_end = time.time()
+    ga_fitnesses.append(best_ga_fitness)
+    ga_times.append(sub_end - sub_start)
+
+    ## MIMIC
+    sub_start = time.time()
+    best_m_state, best_m_fitness, m_curve = mlr.mimic(problem, pop_size=200, 
+                                                    max_attempts=attemps, 
+                                                    random_state=seed, curve=True)                                                   
+    sub_end = time.time()
+    m_fitnesses.append(best_m_fitness)
+    m_times.append(sub_end - sub_start)
+
+end = time.time()
+print(f"\n\nTotal Time Elapsed: {end-start}\n")
+
+# Plotting
+print("Plotting Part 2")
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(1,1,1)
+ax2.plot(test_range,rhc_fitnesses)
+ax2.plot(test_range,sa_fitnesses)
+ax2.plot(test_range,ga_fitnesses)
+ax2.plot(test_range,m_fitnesses)
+
+plt.legend()
+plt.show()
+plt.savefig("Opt1Part2Fitnesses.png")
+
+fig3 = plt.figure()
+ax3 = fig3.add_subplot(1,1,1)
+
+ax3.plot(test_range,rhc_times)
+ax3.plot(test_range,sa_times)
+ax3.plot(test_range,ga_times)
+ax3.plot(test_range,m_times)
+
+plt.legend()
+plt.show()
+plt.savefig("Opt1Part2Times.png")
