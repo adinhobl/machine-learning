a87a7a82d2dc8decbce8f9bfd99114f47fe1c7dd
adinhobl <andrewdinhobl@gmail.com>
2020-10-06 08:37:22 -0500

    some updates to NN

--- randomized-opt/nn.py
+++ randomized-opt/nn.py
@@ -10,7 +10,7 @@ from sklearn.metrics import accuracy_score
 
 seed = 123
 
-## Setup
+## Data
 print("######### Setup #########")
 data = pd.read_csv("randomized-opt/NN/data.csv")
 data["Class"] = data["Class"].map({"RB":1, "NRB":0})
@@ -32,24 +32,34 @@ X_test_std = scaler.transform(X_test)
 
 print(f"Data Sizes: \n  X_train: {X_train_std.shape}\n  X_valid: {X_valid_std.shape}\n  X_test: {X_test.shape}\n  y_train: {y_train.shape}\n  y_valid: {y_valid.shape}\n  y_test: {y_test.shape}\n")
 
+## Setup
+ga_pop = 400
+ga_mut = 0.325
 hidden_nodes = [132]
 iters = [10,50,100,300,500,800,1000,2000,5000]
 final_its = 10000
+rhc_losses = []
+rhc_times = []
+rhc_train = []
+rhc_valid = []
+sa_losses = []
+sa_times = []
+sa_train = []
+sa_valid = []
+ga_losses = []
+ga_times = []
+ga_train = []
+ga_valid = []
 
 for i in iters:
     print(f"Iters: {i}")
 
     #randomized hill climbing
-    rhc_losses = []
-    rhc_times = []
-    rhc_train = []
-    rhc_valid = []
-
     nn_rhc = mlr.NeuralNetwork(hidden_nodes=hidden_nodes, activation='sigmoid', curve=False, 
                             algorithm='random_hill_climb', max_iters=i, random_state=seed,
                             learning_rate=1.7,
                             early_stopping=False,
-                            restarts=100)
+                            restarts=200)
 
     start = time.time()
     nn_rhc.fit(X_train_std, y_train)
@@ -68,14 +78,10 @@ for i in iters:
     print(f"  RHC Valid Acc: {valid_acc}")
 
     # simulated annealing
-    sa_losses = []
-    sa_times = []
-    sa_train = []
-    sa_valid = []
-
-    sch = mlr.GeomDecay() #mlr.ArithDecay mlr.ExpDecay
+    mint = 0.001; maxt = 2; r = (mint/maxt)**(1/i)
+    sch = mlr.GeomDecay(maxt, r, mint) #mlr.ArithDecay mlr.ExpDecay
     nn_sa = mlr.NeuralNetwork(hidden_nodes=hidden_nodes, activation='sigmoid', curve=False,
-                            algorithm='random_hill_climb', max_iters=i, random_state=seed,
+                            algorithm='simulated_annealing', max_iters=i, random_state=seed,
                             learning_rate=3.57,
                             early_stopping=False,
                             schedule=sch)
@@ -96,17 +102,12 @@ for i in iters:
     print(f"  SA Valid Acc: {valid_acc}")
 
     # genetic algorithm
-    ga_losses = []
-    ga_times = []
-    ga_train = []
-    ga_valid = []
-
     nn_ga = mlr.NeuralNetwork(hidden_nodes=hidden_nodes, activation='sigmoid', curve=False,
-                            algorithm='random_hill_climb', max_iters=i, random_state=seed,
+                            algorithm='genetic_alg', max_iters=i, random_state=seed,
                             learning_rate=2.106,
                             early_stopping=False,
-                            pop_size=400,
-                            mutation_prob= 0.2)
+                            pop_size=ga_pop,
+                            mutation_prob= ga_mut)
 
     start = time.time()
     nn_ga.fit(X_train_std, y_train)
@@ -155,10 +156,10 @@ print(f"Valid Accuracy:\n  {valid_acc}")
 
 # simulated annealing
 print("\n\n######### SA #########")
-
-sch = mlr.GeomDecay() #mlr.ArithDecay mlr.ExpDecay
+mint = 0.001; maxt = 2; r = (mint/maxt)**(1/final_its)
+sch = mlr.GeomDecay(maxt, r, mint) #mlr.ArithDecay mlr.ExpDecay
 nn_sa = mlr.NeuralNetwork(hidden_nodes=hidden_nodes, activation='sigmoid', curve=True,
-                        algorithm='random_hill_climb', max_iters=final_its, random_state=seed,
+                        algorithm='simulated_annealing', max_iters=final_its, random_state=seed,
                         learning_rate=3.57,
                         early_stopping=False,
                         schedule=sch)
@@ -184,11 +185,11 @@ print(f"Valid Accuracy:\n  {valid_acc}")
 print("\n\n######### GA #########")
 
 nn_ga = mlr.NeuralNetwork(hidden_nodes=hidden_nodes, activation='sigmoid', curve=True,
-                        algorithm='random_hill_climb', max_iters=final_its, random_state=seed,
+                        algorithm='genetic_alg', max_iters=final_its, random_state=seed,
                         learning_rate=2.106,
                         early_stopping=False,
-                        pop_size=400,
-                        mutation_prob= 0.2)
+                        pop_size=ga_pop,
+                        mutation_prob= ga_mut)
 
 nn_ga.fit(X_train_std, y_train)
 train_pred = nn_ga.predict(X_train_std)
@@ -208,7 +209,7 @@ print(f"Valid Accuracy:\n  {valid_acc}")
 
 
 ## Plotting
-print("Plotting\n")
+print("\nPlotting\n")
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(1,1,1)
 ax1.plot(nn_rhc.fitness_curve, label="Randomized Hill Climbing")
@@ -217,24 +218,24 @@ ax1.plot(nn_ga.fitness_curve, label="Genetic Algorithm")
 
 ax1.set_xlabel("Iterations")
 ax1.set_ylabel("Loss")
-ax1.set_title(f"Loss vs. Iterations")
+ax1.set_title("Loss vs. Iterations")
 
 plt.legend()
-plt.savefig("NN/FinalIt.png")
+plt.savefig("randomized-opt/NN/FinalIt.png")
 
 iters.append(final_its)
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(1,1,1)
-ax2.plot(iters,rhc_losses, label="Randomized Hill Climbing")
-ax2.plot(iters,sa_losses, label="Simulated Annealing")
-ax2.plot(iters,ga_losses, label="Genetic Algorithm")
+ax2.plot(iters, rhc_losses, label="Randomized Hill Climbing")
+ax2.plot(iters, sa_losses, label="Simulated Annealing")
+ax2.plot(iters, ga_losses, label="Genetic Algorithm")
 
-ax2.set_xlabel("Problem Size")
+ax2.set_xlabel("Iterations")
 ax2.set_ylabel("Final Fitness")
-ax2.set_title(f"Final Fitness vs. Problem Size")
+ax2.set_title(f"Final Fitness vs. Iterations")
 
 plt.legend()
-plt.savefig("NN/Part2losses.png")
+plt.savefig("randomized-opt/NN/Part2losses.png")
 
 fig3 = plt.figure()
 ax3 = fig3.add_subplot(1,1,1)
@@ -243,12 +244,25 @@ ax3.plot(iters,rhc_times, label="Randomized Hill Climbing")
 ax3.plot(iters,sa_times, label="Simulated Annealing")
 ax3.plot(iters,ga_times, label="Genetic Algorithm")
 
-ax3.set_xlabel("Problem Size")
+ax3.set_xlabel("Iterations")
 ax3.set_ylabel("Runtime (s)")
-ax3.set_title(f"Runtime vs. Problem Size")
+ax3.set_title(f"Runtime vs. Iterations")
 
 plt.legend()
-plt.savefig("NN/Part2Times.png")
+plt.savefig("randomized-opt/NN/Part2Times.png")
+
+fig4 = plt.figure()
+ax4 = fig4.add_subplot(1,1,1)
+ax4.plot(iters, rhc_valid, label="Randomized Hill Climbing")
+ax4.plot(iters, sa_valid, label="Simulated Annealing")
+ax4.plot(iters, ga_valid, label="Genetic Algorithm")
+
+ax4.set_xlabel("Iterations")
+ax4.set_ylabel("Validation Accuracy")
+ax4.set_title(f"Validation Accuracy vs. Iterations")
+
+plt.legend()
+plt.savefig("randomized-opt/NN/Part2valid.png")
 
 # ## Saving data
 # part2df = pd.DataFrame()
@@ -261,7 +275,7 @@ plt.savefig("NN/Part2Times.png")
 # part2df["ga_times"] = ga_times
 # part2df["m_times"] = m_times
 
-# part2df.to_csv("NN/Part2data.csv")
+# part2df.to_csv("randomized-opt/NN/Part2data.csv")
 
 # part1df = pd.DataFrame()
 # part1df["rhc_curve"] = rhc_curve
@@ -269,4 +283,4 @@ plt.savefig("NN/Part2Times.png")
 # part1df["ga_curve"] = ga_curve
 # part1df["m_curve"] = m_curve
 
-# part1df.to_csv("NN/Part1data.csv")
+# part1df.to_csv("randomized-opt/NN/Part1data.csv")
