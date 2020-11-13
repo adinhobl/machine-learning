285d0600d83db1362b4fa3ff8fe9755f39477f09
adinhobl <andrewdinhobl@gmail.com>
2020-11-12 22:43:28 -0600

    typed out gridworld

--- reinforcement-learning/mdp.jl
+++ reinforcement-learning/mdp.jl
@@ -1,14 +1,26 @@
+using Plots
 using POMDPs
 using POMDPModels
 using POMDPPolicies
+using POMDPSimulators
+using TabularTDLearning
+
+## Small, non tabular - Tiger POMDP
 
 
-# Small, non tabular - Tiger POMDP
+# Value Iteration
+
+
+# Policy Iteration
+
+
+# Q-Learning
+exppolicy = EpsGreedyPolicy(pomdp, 0.01)
+solver = QLearningSolver(exploration_policy=exppolicy, n_episodes=5000, max_episode_length=50, learning_rate=0.1, eval_every=50, n_eval_traj=100, verbose=true)
+policy = solve(solver, pomdp)
 
 
 
 
 
-
-
-# Large, tabular - 
+## Large, tabular - 
